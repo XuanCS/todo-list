bf18faddbb14892f58116a65c5c50ab6a614103a
Xuan Lu <xuanlubio@gmail.com>
2020-06-25 11:14:21 -0400

    implement Todo list react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+
+const INIT_TODOS = [
+  {
+    text: 'Learn Reacts',
+    isComplete: false,
+  },
+  {
+    text: 'Meet Friends',
+    isComplete: false,
+  },
+  {
+    text: 'Build Apps',
+    isComplete: false,
+  },
+]
 
 function App() {
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || INIT_TODOS);
+
+  useEffect (() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = text => {
+    const newTodos = [{text}, ...todos];
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (e, index) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isComplete = true;
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        <TodoForm addTodo={addTodo} />
+        {
+          todos.map((item, index) => (
+            <Todo
+              key={item.text}
+              todo={item}
+              index={index}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))
+        }
+      </div>
     </div>
   );
 }
